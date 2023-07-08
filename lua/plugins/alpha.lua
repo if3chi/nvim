e8ce12f895670d9b673c1b821d42b6c53d74b477
If3chi <j.cyxtus@gmail.com>
2023-07-08 23:37:54 +0000

    Updated dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,8 +2,15 @@ return {
   "goolord/alpha-nvim",
   opts = function(_, opts)
     local logo = [[
-              [ @ifeabasi ]
+ ___   __  _____        _      _ 
+|_ _| / _||___ /   ___ | |__  (_)
+ | | | |_   |_ \  / __|| '_ \ | |
+ | | |  _| ___) || (__ | | | || |
+|___||_|  |____/  \___||_| |_||_|
+                                 
+
+         [ @ifeabasi ]
     ]]
-    opts.section.header.val = vim.split(logo, "\n", { trimepty = true })
+    opts.section.header.val = vim.split(logo, "\n", { trimemtpy = true })
   end,
 }
