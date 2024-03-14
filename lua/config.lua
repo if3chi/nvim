c1f4ae8d926639bfbdba9672a7647f1fc6ee87c5
if3chi <j.cyxtus@gmail.com>
2024-03-14 02:48:52 +0000

    Added completion, dashoard, none-ls for formatting and updated config

--- lua/config.lua
+++ lua/config.lua
@@ -1,6 +1,11 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+vim.opt.showmode = false
+vim.opt.number = true
+
+-- TODO: add vim key binding.
