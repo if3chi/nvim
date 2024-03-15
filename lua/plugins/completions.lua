368ae1991667ec27e65fe46e62213dad9c5c1a05
if3chi <j.cyxtus@gmail.com>
2024-03-15 04:19:07 +0000

    Updated config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,7 +2,6 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
     {
       "L3MON4D3/LuaSnip",
       build = (function()
@@ -16,10 +15,6 @@ return {
       end)(),
     },
     "saadparwaiz1/cmp_luasnip",
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "rafamadriz/friendly-snippets",
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,20 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.completion.spell,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.completion.spell,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, { desc = "[F]ormat [D]ocument" })
-  end,
+		vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, { desc = "[F]ormat [D]ocument" })
+	end,
 }
