107419582663f1a05fc7cd4c897f8cfefb48d9f8
if3chi <j.cyxtus@gmail.com>
2024-05-31 14:11:55 +0000

    Updated mapping n configs

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end, { desc = "[A]dd to harpoon list" })
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<C-e>", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end, { desc = "Toggle [H]arpoon List" })
 
