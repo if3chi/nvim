9cdb87d321a30b783c5e88c9b468b55a0e69748c
if3chi <j.cyxtus@gmail.com>
2024-06-05 01:38:55 +0000

    partially configured treesitter for blade

---
+++ after/queries/blade/highlights.scm
@@ -0,0 +1,9 @@
+(directive) @function @tag
+(directive_start) @function @tag
+(directive_end) @function @tag
+(comment) @comment @spell
+((parameter) @include (#set! "priority" 110))
+((php_only) @include (#set! "priority" 110))
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
+(keyword) @function
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,70 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "javascript", "php" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			-- @todo: configure plugin
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		opts = {
+			textobjects = { -- 'nvim-treesitter/nvim-treesitter-textobjects',
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["if"] = "@function.inner",
+						["af"] = "@function.outer",
+						["ic"] = "@class.inner",
+						["ac"] = "@class.outer",
+						["il"] = "@loop.inner",
+						["al"] = "@loop.outer",
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			-- https://github.com/EmranMR/tree-sitter-blade/discussions/19#discussion-5400675
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+			---@diagnostic disable-next-line: inject-field
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = { "src/parser.c" },
+					branch = "main",
+					generate_requires_npm = true,
+					requires_generate_from_grammar = true,
+				},
+				filetype = "blade",
+			}
+
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup(vim.tbl_extend("keep", opts, {
+				ensure_installed = {
+					"bash",
+					"c",
+					"html",
+					"lua",
+					"markdown",
+					"vim",
+					"vimdoc",
+					"javascript",
+					"php",
+					"php_only",
+					"python",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			}))
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
