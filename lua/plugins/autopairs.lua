ecdd50f6cdb33a3c1c2dc197d66fbaa86a3eed95
if3chi <j.cyxtus@gmail.com>
2024-07-06 09:47:25 +0000

    Switched to nvim-autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,16 @@
+-- autopairs
+-- https://github.com/windwp/nvim-autopairs
+
 return {
-	"echasnovski/mini.pairs",
+	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	opts = {
-		mappings = {
-			["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-		},
-	},
+	-- Optional dependency
+	dependencies = { "hrsh7th/nvim-cmp" },
+	config = function()
+		require("nvim-autopairs").setup({})
+		-- If you want to automatically add `(` after selecting a function or method
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp = require("cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
